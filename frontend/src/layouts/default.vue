bc9f0a809567ab0dcd164c49ba0160ce47aaacee
taihei.okawa <taihei.okawa@vitalize.co.jp>
2023-01-05 04:00:34 +0900

    golang-sync

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -106,7 +106,12 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
-        }
+        },
+        {
+          icon: 'mdi-account',
+          title: 'test',
+          to: '/test'
+        },
       ],
       miniVariant: false,
       right: true,
